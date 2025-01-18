183d67c9c9af85e144149aa024778cc9ac51ac92
lwmacct <lwmacct@icloud.com>
2025-01-18 20:51:07 +0800

    优化 TsFlag 结构体，添加数据库配置和任务位掩码

--- app/app.go
+++ app/app.go
@@ -2,5 +2,5 @@ package app
 
 import "github.com/lwmacct/241224-go-template-pkgs/m_log"
 
-var Flag *TsFlag = &TsFlag{}
+var Flag *TsFlag = new(TsFlag)
 var Log *m_log.Ts = m_log.New(nil)
--- app/flag.go
+++ app/flag.go
@@ -3,13 +3,15 @@ package app
 import "github.com/lwmacct/241224-go-template-pkgs/m_log"
 
 type TsFlag struct {
-	Log    m_log.Config
-	Start  struct{} `group:"start" note:"默认配置"`
-	Client struct {
-		Name string `group:"client" note:"客户端名称(ID)" default:""`
+	Log   m_log.Config
+	Start struct{} `group:"start" note:"默认配置"`
+
+	DB struct {
+		PGSQL string `group:"db" note:"PostgreSQL URL" default:"postgres://username:password@localhost:5432/dbname?sslmode=disable&TimeZone=Asia/Shanghai"`
+		Redis string `group:"db" note:"Redis URL" default:"redis://user:password@localhost:6379/0?protocol=3"` // https://github.com/redis/go-redis/blob/91dddc2e1108c779e8c5b85fd667029873c95172/options.go#L247
 	}
 
-	Server struct {
-		Listener string `group:"server" note:"监听地址" default:"0.0.0.0:8080"`
+	Task struct {
+		Bitmask int `group:"task" note:"任务位掩码" default:"0"`
 	}
 }
